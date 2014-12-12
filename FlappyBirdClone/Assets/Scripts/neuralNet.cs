87b0a4580c7e927ce67fd1605b1bcccbed039f7e
Andrew Ou <clawofbeta@gmail.com>
2014-12-12 14:39:17 -0500

    Basic Neural Network

--- FlappyBirdClone/Assets/Scripts/neuralNet.cs
+++ FlappyBirdClone/Assets/Scripts/neuralNet.cs
@@ -3,13 +3,11 @@ using System.Collections;
 
 public class neuralNet : MonoBehaviour {
 
-	public float wy;
-	public float wx;
+	public float wy; //Y DISTANCE FROM SWEETSPOT TO PIPE, AKA HEIGHT OF PIPE - HEIGHT OF SWEETSPOT)
+	public float wx; //X DISTANCE FROM SWEETSPOT TO PIPE, AKA LENGTH FROM PIPE TO BIRD - LENGTH FROM SWEETSPOT TO BIRD)
 
 	// Use this for initialization
 	void Start () {
-		wy += Random.Range(-.01f,0.1f);
-		wx += Random.Range(-.01f,0.1f);
 	}
 	
 	// Update is called once per frame
@@ -17,40 +15,46 @@ public class neuralNet : MonoBehaviour {
 		
 	}
 
+	//returns 1 if the horizontal distance from bird to sweetspot is less than
+	//the horizontal distance from bird to pipe, 0 otherwise
+	//if distance from bird to sweepspot is negative, though, always return 0
+	public int closerSweetSpot(float x_input){
+		float bird_to_spot = x_input - wx;
+		if (wx + x_input < 0)
+			return 0;
+		else if (bird_to_spot < x_input){
+			return 1;
+		}
+		else
+			return 0;
+		}
+	// 
+	public int isLowerThanPipe(float y_input){
+		if (y_input > 0)
+			return 0;
+		else
+			return 1;
+	}
+
+	public int isLowerThanSweetSpot(float y_input){
+		if (y_input + wy > 0)
+			return 0;
+		else
+			return 1;
+	}
+
+	public bool SweetSpotOrPipe(int LowerPipe, int LowerSweetSpot, int closer){
+		return ((LowerSweetSpot * closer) + (LowerPipe * (1 - closer) > 0));
+		}
+
 	public bool compute(float x_input, float y_input){
 
-		float y_out = func_y(y_input);
-		float x_out = func_x(x_input);
+		int LowerPipe = isLowerThanPipe (y_input);
+		int LowerSweetSpot = isLowerThanSweetSpot (y_input);
+		int closer = closerSweetSpot (x_input);
 
-		if(x_out * wx + y_out * wy > 0.2f){
-			return true;
-		}
-		else{
-			return false;
-		}
+		return SweetSpotOrPipe (LowerPipe, LowerSweetSpot, closer);
+
 
 	}
-
-	float func_x(float input){
-		if(input > 1){
-			return 1;
-		}
-		else{
-			return 0;
-		}
-	}
-
-	float func_y(float input){
-
-
-		if(input > 0.2f){
-			return 1;
-		}
-		else{
-			return 0;
-		}
-
-	}
-	
-
 }
